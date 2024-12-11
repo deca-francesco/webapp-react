c12a5f9db81a77ed5b670e49d70f628d99e4f257
deca-francesco <francescodecarlo70@gmail.com>
2024-12-11 17:28:17 +0100

    ADD: my_card css class

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,17 +6,15 @@ export default function MovieCard({ movie }) {
 
     return (
         <div className="col">
-            <div className="card my_movie_card">
-                <div className="card-body">
-                    <h4>{movie.title}</h4>
-                    <p><strong>{movie.director}</strong></p>
-                    <p>{movie.release_year}</p>
-                    <p>{movie.genre}</p>
-                    <p>{movie.abstract}</p>
-                    <Link to={`/movies/${movie.id}`}>
-                        <button className="btn btn-primary">Details</button>
-                    </Link>
-                </div>
+            <div className="my_movie_card">
+                <h4>{movie.title}</h4>
+                <p><strong>{movie.director}</strong></p>
+                <p>{movie.release_year}</p>
+                <p>{movie.genre}</p>
+                <p>{movie.abstract}</p>
+                <Link to={`/movies/${movie.id}`}>
+                    <button className="btn btn-primary">Details</button>
+                </Link>
             </div>
         </div>
     )
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -23,14 +23,12 @@ export default function MovieDetailsPage({ api_server, end_point }) {
         <>
             <div className="container">
                 <h2>Movie details:</h2>
-                <div className="card mb-4">
-                    <div className="card-body">
-                        <h4><strong>Title: </strong>{movieDetails.title}</h4>
-                        <p><strong>Director: </strong>{movieDetails.director}</p>
-                        <p><strong>Year: </strong>{movieDetails.release_year}</p>
-                        <p><strong>Genre: </strong>{movieDetails.genre}</p>
-                        <p><strong>Abstract: </strong>{movieDetails.abstract}</p>
-                    </div>
+                <div className="my_movie_details_card mb-4">
+                    <h4><strong>Title: </strong>{movieDetails.title}</h4>
+                    <p><strong>Director: </strong>{movieDetails.director}</p>
+                    <p><strong>Year: </strong>{movieDetails.release_year}</p>
+                    <p><strong>Genre: </strong>{movieDetails.genre}</p>
+                    <p><strong>Abstract: </strong>{movieDetails.abstract}</p>
                 </div>
                 <h5>Reviews:</h5>
                 <div className="row row-cols-1 g-3">
